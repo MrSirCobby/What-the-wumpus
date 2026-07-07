58b0f07eaa0a9ef8db2e62ff3150f456a51767e5
MrSirCobby <colby.hugo@education.nsw.gov.au>
2026-07-07 13:20:30 +1000

    PLAYER ANIMATION FIXED

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import pygame, sys
+import pygame
 from settings import *
 from player import *
-<<<<<<< HEAD
-from rooms import *
-=======
 
->>>>>>> 18ac6ef0572c0686cbc94729af41101726dd867f
 pygame.init()
 
 #create game window
@@ -19,25 +15,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-<<<<<<< HEAD
-    screen.fill((0, 0, 0)) #resets the screen to black every frame
-
-    button_pressed = pygame.key.get_pressed() #sets button_pressed to all keys that are pressed
-    player_action(button_pressed) #determines the players action based on the buttons pressed
-    #print(player_position) debugging line to print the players position to the console
-
-
-    pygame.draw.circle(screen, (255, 255, 255), (player_position[0], player_position[1]), 30) #placeholder for the player, draws a white circle at the players position
-    
-    
-
-
-    pygame.display.update()
-    clock.tick(FPS)
-    
-
-
-=======
 
     button_pressed = pygame.key.get_pressed()
     button_action(button_pressed)
@@ -45,7 +22,6 @@ while running:
     print(player_position)
 
     screen.fill((0, 0, 0))
->>>>>>> 18ac6ef0572c0686cbc94729af41101726dd867f
 
     #player animation
     player_image = moving_animation()
